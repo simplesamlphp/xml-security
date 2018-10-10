150e5ab53ea704e03ba8a4bb4ac83e38ca733570
Willem Stuursma-Ruwen <willem@mollie.nl>
2018-10-10 09:10:08 +0200

    Fix casing of class name

--- src/XMLSecEnc.php
+++ src/XMLSecEnc.php
@@ -470,7 +470,7 @@ class XMLSecEnc
                     }
                     $id = substr($uri, 1);
 
-                    $query = "//xmlsecenc:EncryptedKey[@Id='$id']";
+                    $query = '//xmlsecenc:EncryptedKey[@Id="'.XPath::filterAttrValue($id, XPath::DOUBLE_QUOTE).'"]';
                     $keyElement = $xpath->query($query)->item(0);
                     if (!$keyElement) {
                         throw new Exception("Unable to locate EncryptedKey with @Id='$id'.");
