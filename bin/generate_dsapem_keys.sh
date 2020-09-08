1fd4226b985fc682e058079cedfd6b4c767a7da0
Tim van Dijen <tvdijen@gmail.com>
2020-09-08 21:10:52 +0200

    Import PEMCertificatesMock

---
+++ bin/generate_dsapem_keys.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+
+DESTINATION="/tmp"
+PASSWORD="1234"
+KEYSIZE="1024"
+VALID_UNTIL=36500
+
+if [[ $1 == '-h' || $1 == '--help' ]]; then
+    echo "Usage: $0 <destination directory>"
+    exit
+fi
+if [[ $1 == '-d' || $1 == '--destination' ]]; then
+    DESTINATION="${2:-/tmp}"
+fi
+
+PEM_DSA_DIR="${DESTINATION}/resources/certificates/dsa-pem"
+
+declare -a CERTS=(
+  "signed"
+  "other"
+  "expired"
+  "corrupted"
+  "broken"
+)
+
+## Generate directory structure ##
+
+mkdir $DESTINATION/certificates
+touch $DESTINATION/index.txt
+echo 01 > $DESTINATION/serial.txt
+mkdir $PEM_DSA_DIR
+
+## Generate config file
+
+cat>/tmp/ca.conf<<'EOF'
+[ ca ]
+default_ca = ca_default
+
+[ ca_default ]
+dir = REPLACE_LATER
+new_certs_dir = $dir/certificates/dsa-pem
+database = /tmp/index.txt
+serial = /tmp/serial.txt
+default_md = sha256
+policy = generic_policy
+
+[ generic_policy ]
+countryName = optional
+stateOrProvinceName = optional
+localityName = optional
+organizationName = optional
+organizationalUnitName = optional
+commonName = supplied
+emailAddress = optional
+EOF
+
+sed -i "s|REPLACE_LATER|${DESTINATION}|" ${DESTINATION}/ca.conf
+
+## Generate dsaparam file
+openssl dsaparam -out $PEM_DSA_DIR/dsaparam.pem 1024
+DSA_PARAM="${PEM_DSA_DIR}/dsaparam.pem"
+
+## Create CA ##
+
+SUBJ="/emailAddress=noreply@simplesamlphp.org/CN=SimpleSAMLphp\ Testing\ CA/O=SimpleSAMLphp\ HQ/L=Honolulu/ST=Hawaii/C=US"
+
+openssl req -x509 -newkey dsa:$DSA_PARAM -keyout $PEM_DSA_DIR/simplesamlphp.org-ca_nopasswd.key \
+  -out $PEM_DSA_DIR/simplesamlphp.org-ca.crt -subj "${SUBJ}" -nodes
+
+openssl dsa -aes256 -in $PEM_DSA_DIR/simplesamlphp.org-ca_nopasswd.key \
+  -out $PEM_DSA_DIR/simplesamlphp.org-ca.key -passout pass:$PASSWORD
+
+## Generate certificates
+
+for CERT in "${CERTS[@]}"
+do
+  SUBJ="/emailAddress=noreply@simplesamlphp.org/CN=${CERT}.simplesamlphp.org/O=SimpleSAMLphp\ HQ/L=Honolulu/ST=Hawaii/C=US"
+
+  openssl req -out $PEM_DSA_DIR/$CERT.simplesamlphp.org.csr -subj "${SUBJ}" -nodes \
+    -keyout $PEM_DSA_DIR/$CERT.simplesamlphp.org_nopasswd.key -newkey dsa:$DSA_PARAM
+
+  openssl dsa -aes256 -in $PEM_DSA_DIR/$CERT.simplesamlphp.org_nopasswd.key \
+    -out $PEM_DSA_DIR/$CERT.simplesamlphp.org.key -passout pass:$PASSWORD
+
+  if [[ "$CERT" == "expired" ]]; then
+    openssl ca -keyfile $PEM_DSA_DIR/simplesamlphp.org-ca_nopasswd.key -cert $PEM_DSA_DIR/simplesamlphp.org-ca.crt \
+      -in $PEM_DSA_DIR/$CERT.simplesamlphp.org.csr -out $PEM_DSA_DIR/$CERT.simplesamlphp.org.crt -enddate $(date '+%y%m%d%H%M%SZ') \
+      -config /tmp/ca.conf -batch -subj "${SUBJ}"
+  else
+    openssl ca -keyfile $PEM_DSA_DIR/simplesamlphp.org-ca_nopasswd.key -cert $PEM_DSA_DIR/simplesamlphp.org-ca.crt \
+      -in $PEM_DSA_DIR/$CERT.simplesamlphp.org.csr -out $PEM_DSA_DIR/$CERT.simplesamlphp.org.crt \
+      -days $VALID_UNTIL -config /tmp/ca.conf -batch -subj "${SUBJ}"
+  fi
+
+  sed -i '/-----BEGIN CERTIFICATE-----/,$!d' $PEM_DSA_DIR/$CERT.simplesamlphp.org.crt
+done
+
+## Self-signed certificate
+
+SUBJ="/emailAddress=noreply@simplesamlphp.org/CN=selfsigned.simplesamlphp.org/O=SimpleSAMLphp\ HQ/L=Honolulu/ST=Hawaii/C=US"
+
+openssl req -x509 -newkey dsa:$DSA_PARAM -keyout $PEM_DSA_DIR/selfsigned.simplesamlphp.org_nopasswd.key \
+  -out $PEM_DSA_DIR/selfsigned.simplesamlphp.org.crt -subj "${SUBJ}" -nodes
+
+openssl dsa -aes256 -in $PEM_DSA_DIR/selfsigned.simplesamlphp.org_nopasswd.key \
+  -out $PEM_DSA_DIR/selfsigned.simplesamlphp.org.key -passout pass:$PASSWORD
+
+## Corrupt certicate ##
+
+# This sed-command will swap the first & last character
+sed 's/^\(.\)\(.\+\)\(.\)$/\3\2\1/' $PEM_DSA_DIR/corrupted.simplesamlphp.org_nopasswd.key \
+  > $PEM_DSA_DIR/corrupted.simplesamlphp.org.key
+mv $PEM_DSA_DIR/corrupted.simplesamlphp.org.crt $PEM_DSA_DIR/corrupted.simplesamlphp.org.tmp
+sed 's/^\(.\)\(.\+\)\(.\)$/\3\2\1/' $PEM_DSA_DIR/corrupted.simplesamlphp.org.tmp \
+  > $PEM_DSA_DIR/corrupted.simplesamlphp.org.crt
+rm $PEM_DSA_DIR/corrupted.simplesamlphp.org_nopasswd.key
+
+## Break PEM structure ##
+
+# This sed-command will swap the first & last character
+sed 's/ //g' $PEM_DSA_DIR/broken.simplesamlphp.org_nopasswd.key > $PEM_DSA_DIR/broken.simplesamlphp.org.key
+mv $PEM_DSA_DIR/broken.simplesamlphp.org.crt $PEM_DSA_DIR/broken.simplesamlphp.org.tmp
+sed 's/ //g' $PEM_DSA_DIR/broken.simplesamlphp.org.tmp > $PEM_DSA_DIR/broken.simplesamlphp.org.crt
+rm $PEM_DSA_DIR/broken.simplesamlphp.org_nopasswd.key
+
+## Clean-up csr- and ca-files
+rm -f $PEM_DSA_DIR/*.csr
+rm -f $DESTINATION/ca.conf
+rm -f $DESTINATION/index.txt*
+rm -f $DESTINATION/serial.txt*
+rm -f $PEM_DSA_DIR/*.pem
+rm -f $PEM_DSA_DIR/*.tmp
---
+++ src/TestUtils/PEMCertificatesMock.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XMLSecurity\TestUtils;
+
+use Exception;
+use SimpleSAML\XMLSecurity\XMLSecurityKey;
+
+/**
+ * Class \SimpleSAML\TestUtils\PEMCertificatesMock
+ */
+class PEMCertificatesMock
+{
+    public const ALG_SIG_RSA = 'rsa';
+    public const ALG_SIG_DSA = 'dsa';
+
+    public const PUBLIC_KEY_PATTERN = '/^-----BEGIN CERTIFICATE-----([^-]*)^-----END CERTIFICATE-----/m';
+    public const PRIVATE_KEY_PATTERN = '/^-----BEGIN RSA PRIVATE KEY-----([^-]*)^-----END RSA PRIVATE KEY-----/m';
+
+    public const CERTIFICATE_DIR_RSA = '/resources/certificates/rsa-pem';
+    public const CERTIFICATE_DIR_DSA = '/resources/certificates/dsa-pem';
+
+    public const PUBLIC_KEY = 'signed.simplesamlphp.org.crt';
+    public const PRIVATE_KEY = 'signed.simplesamlphp.org_nopasswd.key';
+    public const PRIVATE_KEY_PROTECTED = 'signed.simplesamlphp.org.key';
+    public const OTHER_PUBLIC_KEY = 'other.simplesamlphp.org.crt';
+    public const OTHER_PRIVATE_KEY = 'other.simplesamlphp.org_nopasswd.key';
+    public const OTHER_PRIVATE_KEY_PROTECTED = 'other.simplesamlphp.org.key';
+    public const SELFSIGNED_PUBLIC_KEY = 'selfsigned.simplesamlphp.org.crt';
+    public const SELFSIGNED_PRIVATE_KEY = 'selfsigned.simplesamlphp.org_nopasswd.key';
+    public const SELFSIGNED_PRIVATE_KEY_PROTECTED = 'selfsigned.simplesamlphp.org.key';
+    public const BROKEN_PUBLIC_KEY = 'broken.simplesamlphp.org.crt';
+    public const BROKEN_PRIVATE_KEY = 'broken.simplesamlphp.org.key';
+    public const CORRUPTED_PUBLIC_KEY = 'corrupted.simplesamlphp.org.crt';
+    public const CORRUPTED_PRIVATE_KEY = 'corrupted.simplesamlphp.org.key';
+
+    /**
+     * @param string $key The PEM-encoded key
+     * @param bool $private
+     * @return string The stripped key
+     */
+    private static function stripHeaders(string $key, bool $private)
+    {
+        $matches = [];
+        if ($private === false && !preg_match(self::PUBLIC_KEY_PATTERN, $key, $matches)) {
+            throw new Exception('Could not find PEM encoded certificate.');
+        } elseif ($private === true && !preg_match(self::PRIVATE_KEY_PATTERN, $key, $matches)) {
+            throw new Exception('Could not find PEM encoded key.');
+        }
+
+        /** @psalm-suppress EmptyArrayAccess */
+        return preg_replace('/\s+/', '', $matches[1]);
+    }
+
+
+    /**
+     * @param string $file The file we should load
+     * @param string $sig_alg  One of rsa|dsa
+     * @return string The file contents
+     */
+    public static function loadPlainCertificateFile(string $file, $sig_alg = self::ALG_SIG_RSA)
+    {
+        if ($sig_alg === self::ALG_SIG_RSA) {
+            return file_get_contents(dirname(dirname(__FILE__)) . self::CERTIFICATE_DIR_RSA . DIRECTORY_SEPARATOR . $file);
+        } else {
+            return file_get_contents(dirname(dirname(__FILE__)) . self::CERTIFICATE_DIR_DSA . DIRECTORY_SEPARATOR . $file);
+        }
+    }
+
+
+    /**
+     * @param string $hash_alg
+     * @param string The file to use
+     * @param string $sig_alg  One of rsa|dsa
+     * @return \SimpleSAML\XMLSecurity\XMLSecurityKey
+     */
+    public static function getPublicKey(
+        string $hash_alg,
+        string $file,
+        string $sig_alg = self::ALG_SIG_RSA
+    ): XMLSecurityKey {
+        $publicKey = new XMLSecurityKey($hash_alg, ['type' => 'public']);
+        $publicKey->loadKey(self::getPlainPublicKey($file, $sig_alg));
+        return $publicKey;
+    }
+
+
+    /**
+     * @param string $hash_alg
+     * @param string The file to use
+     * @param string $sig_alg  One of rsa|dsa
+     * @return \SimpleSAML\XMLSecurity\XMLSecurityKey
+     */
+    public static function getPrivateKey(
+        string $hash_alg,
+        string $file,
+        string $sig_alg = self::ALG_SIG_RSA
+    ): XMLSecurityKey {
+        $privateKey = new XMLSecurityKey($hash_alg, ['type' => 'private']);
+        $privateKey->loadKey(self::getPlainPrivateKey($file, $sig_alg));
+        return $privateKey;
+    }
+
+
+    /**
+     * @param string $file The file to use
+     * @param string $sig_alg  One of rsa|dsa
+     * @return string
+     */
+    public static function getPlainPublicKey(
+        string $file = self::PUBLIC_KEY,
+        string $sig_alg = self::ALG_SIG_RSA
+    ): string {
+        return self::loadPlainCertificateFile($file, $sig_alg);
+    }
+
+
+    /**
+     * @param string $file The file to use
+     * @param string $sig_alg  One of rsa|dsa
+     * @return string
+     */
+    public static function getPlainPrivateKey(
+        string $file = self::PRIVATE_KEY,
+        string $sig_alg = self::ALG_SIG_RSA
+    ): string {
+        return self::loadPlainCertificateFile($file, $sig_alg);
+    }
+
+
+    /**
+     * @param string $file The file to use
+     * @param string $sig_alg  One of rsa|dsa
+     * @return string
+     */
+    public static function getPlainPublicKeyContents(
+        string $file = self::PUBLIC_KEY,
+        string $sig_alg = self::ALG_SIG_RSA
+    ): string {
+        return self::stripHeaders(self::loadPlainCertificateFile($file, $sig_alg), false);
+    }
+
+
+    /**
+     * @param string $file The file to use
+     * @param string $sig_alg  One of rsa|dsa
+     * @return string
+     */
+    public static function getPlainPrivateKeyContents(
+        string $file = self::PRIVATE_KEY,
+        string $sig_alg = self::ALG_SIG_RSA
+    ): string {
+        return self::stripHeaders(self::loadPlainCertificateFile($file, $sig_alg), true);
+    }
+}
