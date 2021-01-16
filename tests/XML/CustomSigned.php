de5a171083c8f7922e8f3c621fd96d0c5a1f8f54
Tim van Dijen <tvdijen@gmail.com>
2021-01-16 21:02:58 +0100

    Fix

--- tests/XML/CustomSigned.php
+++ tests/XML/CustomSigned.php
@@ -31,24 +31,10 @@ final class CustomSigned extends AbstractSignedXMLElement
      */
     public static function fromXML(DOMElement $xml): object
     {
-        Assert::same($xml->localName, 'CustomSignable', InvalidDOMElementException::class);
-        Assert::same($xml->namespaceURI, CustomSignable::NS, InvalidDOMElementException::class);
+        $signature = Signature::getChildrenOfClass($xml);
+        Assert::minCount($signature, 1, MissingElementException::class);
+        Assert::maxCount($signature, 1, TooManyElementsException::class);
 
-//var_dump($xml->ownerDocument->saveXML());
-        $signatureElement = XMLUtils::xpQuery($xml, './ds:Signature');
-        Assert::minCount($signatureElement, 1, MissingElementException::class);
-        Assert::minCount($signatureElement, 1, TooManyElementsException::class);
-//        $signature = Signature::getChildrenOfClass($xml);
-//        Assert::minCount($signature, 1, MissingElementException::class);
-//        Assert::minCount($signature, 1, TooManyElementsException::class);
-
-        $clone = clone $signatureElement[0];
-        $signature = Signature::fromXML($clone);
-
-        return new self(
-            $xml,
-            CustomSignable::fromXML($xml),
-            $signature
-        );
+        return new self($xml, array_pop($signature));
     }
 }
