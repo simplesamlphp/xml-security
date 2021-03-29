c363ec8833d34a5912bfb66e7e34d76bed8941ba
Tim van Dijen <tvdijen@gmail.com>
2021-03-29 21:10:14 +0200

    Make SignedElement*::fromXML generic

--- tests/XML/CustomSigned.php
+++ tests/XML/CustomSigned.php
@@ -4,15 +4,7 @@ declare(strict_types=1);
 
 namespace SimpleSAML\XMLSecurity\Test\XML;
 
-use DOMElement;
-use SimpleSAML\Assert\Assert;
-use SimpleSAML\XML\Chunk;
-use SimpleSAML\XML\Exception\MissingElementException;
-use SimpleSAML\XML\Exception\TooManyElementsException;
-use SimpleSAML\XML\Utils as XMLUtils;
-use SimpleSAML\XMLSecurity\XML\ds\Signature;
 use SimpleSAML\XMLSecurity\XML\AbstractSignedXMLElement;
-use SimpleSAML\XMLSecurity\XML\SignedElementInterface;
 use SimpleSAML\XMLSecurity\XML\SignedElementTrait;
 
 /**
@@ -20,21 +12,4 @@ use SimpleSAML\XMLSecurity\XML\SignedElementTrait;
  */
 final class CustomSigned extends AbstractSignedXMLElement
 {
-    use SignedElementTrait;
-
-
-    /**
-     * Create a class from XML
-     *
-     * @param \DOMElement $xml
-     * @return self
-     */
-    public static function fromXML(DOMElement $xml): object
-    {
-        $signature = Signature::getChildrenOfClass($xml);
-        Assert::minCount($signature, 1, MissingElementException::class);
-        Assert::maxCount($signature, 1, TooManyElementsException::class);
-
-        return new self($xml, array_pop($signature));
-    }
 }
