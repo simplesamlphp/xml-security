2a67536648174912fa2d0d834ec19c66beb8355a
Tim van Dijen <tim.dijen@minbzk.nl>
2021-04-02 21:01:03 +0200

    Add XML-representation for ds:Transform element (#11)
    * Add XML-representation for ds:Transform element
    
    * Improve coverage

---
+++ tests/XML/ds/TransformTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\ds;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML2\Constants;
+use SimpleSAML\Test\XML\SerializableXMLTestTrait;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\Utils as XMLUtils;
+use SimpleSAML\XMLSecurity\XML\ds\Transform;
+
+/**
+ * Class \SAML2\XML\ds\TransformTest
+ *
+ * @covers \SimpleSAML\SAML2\XML\ds\Transform
+ * @covers \SimpleSAML\SAML2\XML\ds\AbstractDsElement
+ *
+ * @package simplesamlphp/saml2
+ */
+final class TransformTest extends TestCase
+{
+    use SerializableXMLTestTrait;
+
+
+    /**
+     */
+    public function setUp(): void
+    {
+        $this->testedClass = Transform::class;
+
+        $this->xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(dirname(dirname(__FILE__))) . '/resources/xml/ds_Transform.xml'
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $transform = new Transform(
+            [
+                new Chunk(DOMDocumentFactory::fromString('<some:Chunk>Random</some:Chunk>')->documentElement),
+                new Chunk(DOMDocumentFactory::fromString('<ds:XPath>count(//. | //@* | //namespace::*)</ds:XPath>')->documentElement)
+            ],
+        );
+
+        $document = DOMDocumentFactory::fromString('<root />');
+        /** @psalm-var \DOMElement $document->firstChild */
+        $transformElement = $transform->toXML($document->firstChild);
+
+        $this->assertEquals('http://www.w3.org/TR/1999/REC-xpath-19991116', $transformElement->getAttribute('Algorithm'));
+        $this->assertCount(2, $transformElement->childNodes);
+
+        $this->assertEquals('some:Chunk', $transformElement->childNodes[0]->localName);
+        $this->assertEquals('Random', $transformElement->childNodes[0]->textContent);
+        $this->assertEquals('ds:XPath', $transformElement->childNodes[1]->localName);
+        $this->assertEquals('count(//. | //@* | //namespace::*)', $transformElement->childNodes[1]->textContent);
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($transform)
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $transform = Transform::fromXML($this->xmlRepresentation->documentElement);
+        $this->assertEquals('http://www.w3.org/TR/1999/REC-xpath-19991116', $transform->getAlgorithm());
+
+        $elements = $transform->getElements();
+        $this->assertCount(2, $elements);
+
+        $this->assertInstanceOf(Chunk::class, $elements[0]);
+        $this->assertInstanceOf(Chunk::class, $elements[1]);
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($transform)
+        );
+    }
+
+
+    /**
+     * Adding an empty StatusDetail element should yield an empty element.
+     */
+    public function testMarshallingEmptyElement(): void
+    {
+        $ds_ns = Transform::NS;
+        $transform = new Transform([]);
+        $this->assertEquals(
+            "<ds:Transform xmlns:ds=\"$ds_ns\" Algorithm=\"http://www.w3.org/TR/1999/REC-xpath-19991116\"/>",
+            strval($transform)
+        );
+        $this->assertTrue($transform->isEmptyElement());
+    }
+}
