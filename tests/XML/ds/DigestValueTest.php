55e334d878087722d324790d4c585faf26b3aade
Tim van Dijen <tim.dijen@minbzk.nl>
2021-04-05 19:53:13 +0200

    Add XML-representation for ds:DigestMethod and ds:DigestValue elements (#13)
    * Fix phpdoc (unrelated)
    
    * Add XML-representation for ds:DigestMethod and ds:DigestValue elements
    
    * Add assertion
    
    * Properly marshall elements
    
    * Reduce marshall-tests to minimum

---
+++ tests/XML/ds/DigestValueTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XMLSecurity\Test\XML\ds;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Test\XML\SerializableXMLTestTrait;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XMLSecurity\XML\ds\DigestValue;
+use SimpleSAML\XMLSecurity\XMLSecurityDSig;
+
+/**
+ * Class \SimpleSAML\XMLSecurity\XML\ds\DigestValueTest
+ *
+ * @covers \SimpleSAML\XMLSecurity\XML\ds\AbstractDsElement
+ * @covers \SimpleSAML\XMLSecurity\XML\ds\DigestValue
+ *
+ * @package simplesamlphp/xml-security
+ */
+final class DigestValueTest extends TestCase
+{
+    use SerializableXMLTestTrait;
+
+    /**
+     */
+    protected function setUp(): void
+    {
+        $this->testedClass = DigestValue::class;
+
+        $this->xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(dirname(dirname(dirname(__FILE__)))) . '/tests/resources/xml/ds_DigestValue.xml'
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $digestValue = new DigestValue('/CTj03d1DB5e2t7CTo9BEzCf5S9NRzwnBgZRlm32REI=');
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($digestValue)
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $digestValue = DigestValue::fromXML($this->xmlRepresentation->documentElement);
+
+        $this->assertEquals('/CTj03d1DB5e2t7CTo9BEzCf5S9NRzwnBgZRlm32REI=', $digestValue->getDigest());
+    }
+}
