7681b2c6136ac2d1958f2f7948564b7427e08c14
Tim van Dijen <tvdijen@gmail.com>
2021-04-18 21:52:07 +0200

    Remove implementation for getNamespaceUri and getNamespacePrefix; implemented generically in xml-common

--- src/XML/ds/AbstractDsElement.php
+++ src/XML/ds/AbstractDsElement.php
@@ -19,26 +19,4 @@ abstract class AbstractDsElement extends AbstractXMLElement
 
     /** @var string */
     public const NS_PREFIX = 'ds';
-
-
-    /**
-     * Get the namespace for the element.
-     *
-     * @return string
-     */
-    public static function getNamespaceURI(): string
-    {
-        return static::NS;
-    }
-
-
-    /**
-     * Get the namespace-prefix for the element.
-     *
-     * @return string
-     */
-    public static function getNamespacePrefix(): string
-    {
-        return static::NS_PREFIX;
-    }
 }
