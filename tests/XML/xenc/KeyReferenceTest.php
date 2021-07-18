580901c264474c605515ed72c4e6475964146517
Jaime Pérez Crespo <jaimepc@gmail.com>
2021-07-18 19:05:09 +0200

    Fix tests

--- tests/XML/xenc/KeyReferenceTest.php
+++ tests/XML/xenc/KeyReferenceTest.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace SimpleSAML\XMLSecurity\Test\XML\xenc;
 
-use DOMDocument;
 use PHPUnit\Framework\TestCase;
 use SimpleSAML\Test\XML\SerializableXMLTestTrait;
 use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XMLSecurity\Constants as C;
+use SimpleSAML\XMLSecurity\XML\ds\Transform;
 use SimpleSAML\XMLSecurity\XML\ds\Transforms;
+use SimpleSAML\XMLSecurity\XML\ds\XPath;
 use SimpleSAML\XMLSecurity\XML\xenc\KeyReference;
-use SimpleSAML\XMLSecurity\XMLSecurityDSig;
 
 /**
  * Class \SimpleSAML\XMLSecurity\Test\XML\xenc\KeyReferenceTest
@@ -48,15 +49,13 @@ final class KeyReferenceTest extends TestCase
         $keyReference = new KeyReference(
             '#Encrypted_KEY_ID',
             [
-                Transforms::fromXML(
-                    DOMDocumentFactory::fromString(<<<XML
-    <ds:Transforms xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
-      <ds:Transform Algorithm="http://www.w3.org/TR/1999/REC-xpath-19991116">
-        <ds:XPath xmlns:xenc="http://www.w3.org/2001/04/xmlenc#">self::xenc:EncryptedKey[@Id="example1"]</ds:XPath>
-      </ds:Transform>
-    </ds:Transforms>
-XML
-                    )->documentElement
+                new Transforms(
+                    [
+                        new Transform(
+                            C::XPATH_URI,
+                            new XPath('self::xenc:EncryptedKey[@Id="example1"]')
+                        )
+                    ]
                 )
             ]
         );
