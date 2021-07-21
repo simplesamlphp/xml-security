4fa1e79aaacae6758098d76947e2ff358ec84f4c
Tim van Dijen <tim.dijen@minbzk.nl>
2021-07-21 22:36:10 +0200

    Remove overhead

--- src/XML/ec/AbstractEcElement.php
+++ src/XML/ec/AbstractEcElement.php
@@ -20,26 +20,4 @@ abstract class AbstractEcElement extends AbstractXMLElement
 
     /** @var string */
     public const NS_PREFIX = 'ec';
-
-
-    /**
-     * Get the namespace for the element.
-     *
-     * @return string
-     */
-    public static function getNamespaceURI(): string
-    {
-        return static::NS;
-    }
-
-
-    /**
-     * Get the namespace prefix for the element.
-     *
-     * @return string
-     */
-    public static function getNamespacePrefix(): string
-    {
-        return static::NS_PREFIX;
-    }
 }
