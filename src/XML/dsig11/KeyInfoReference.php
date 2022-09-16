613936df9b726b98d140cd30e821d890ad55f8ad
Tim van Dijen <tvdijen@gmail.com>
2022-09-17 01:18:34 +0200

    Add ds:KeyInfoReference element (#35)
    * Add ds:KeyInfoReference element
    
    * Add assertion
    
    * Update PR & schema validation

---
+++ src/XML/dsig11/KeyInfoReference.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XMLSecurity\XML\dsig11;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XMLSecurity\Constants as C;
+use SimpleSAML\XMLSecurity\Exception\InvalidArgumentException;
+
+/**
+ * Class representing a dsig11:KeyInfoReference element.
+ *
+ * @package simplesamlphp/xml-security
+ */
+final class KeyInfoReference extends AbstractDsig11Element
+{
+    /** @var string $URI */
+    protected string $URI;
+
+    /** @var string|null $Id */
+    protected ?string $Id = null;
+
+
+    /**
+     * Initialize a KeyInfoReference element.
+     *
+     * @param string $URI
+     * @param string|null $Id
+     */
+    public function __construct(string $URI, ?string $Id = null)
+    {
+        $this->setURI($URI);
+        $this->setId($Id);
+    }
+
+
+    /**
+     * Collect the value of the URI-property
+     *
+     * @return string
+     */
+    public function getURI(): string
+    {
+        return $this->URI;
+    }
+
+
+    /**
+     * Set the value of the URI-property
+     *
+     * @param string $URI
+     */
+    private function setURI(string $URI): void
+    {
+        Assert::validURI($URI, SchemaViolationException::class);
+        $this->URI = $URI;
+    }
+
+
+    /**
+     * Collect the value of the Id-property
+     *
+     * @return string|null
+     */
+    public function getId(): ?string
+    {
+        return $this->Id;
+    }
+
+
+    /**
+     * Set the value of the Id-property
+     *
+     * @param string $Id
+     */
+    private function setId(?string $Id): void
+    {
+        Assert::nullOrValidNCName($Id);
+        $this->Id = $Id;
+    }
+
+
+    /**
+     * Convert XML into a KeyInfoReference
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   If the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'KeyInfoReference', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, KeyInfoReference::NS, InvalidDOMElementException::class);
+
+        /** @psalm-var string $URI */
+        $URI = KeyInfoReference::getAttribute($xml, 'URI');
+        $Id = KeyInfoReference::getAttribute($xml, 'Id', null);
+
+        return new static($URI, $Id);
+    }
+
+
+    /**
+     * Convert this KeyInfoReference element to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this KeyInfoReference element to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+        $e->setAttribute('URI', $this->URI);
+
+        if ($this->Id !== null) {
+            $e->setAttribute('Id', $this->Id);
+        }
+
+        return $e;
+    }
+}
---
+++ tests/XML/dsig11/KeyInfoReferenceTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XMLSecurity\Test\XML\dsig11;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Test\XML\SchemaValidationTestTrait;
+use SimpleSAML\Test\XML\SerializableElementTestTrait;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XMLSecurity\Constants as C;
+use SimpleSAML\XMLSecurity\XML\dsig11\KeyInfoReference;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\XMLSecurity\Test\XML\dsig11\KeyInfoReferenceTest
+ *
+ * @covers \SimpleSAML\XMLSecurity\XML\dsig11\AbstractDsig11Element
+ * @covers \SimpleSAML\XMLSecurity\XML\dsig11\KeyInfoReference
+ *
+ * @package simplesamlphp/xml-security
+ */
+final class KeyInfoReferenceTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+    /**
+     */
+    protected function setUp(): void
+    {
+        $this->testedClass = KeyInfoReference::class;
+
+        $this->schema = dirname(dirname(dirname(dirname(__FILE__)))) . '/schemas/xmldsig11-schema.xsd';
+
+        $this->xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(dirname(dirname(dirname(__FILE__)))) . '/tests/resources/xml/dsig11_KeyInfoReference.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $KeyInfoReference = new KeyInfoReference('#_e395489e5f8444f1aabb4b2ca98a23b793d211ddf0', 'abc123');
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($KeyInfoReference),
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $KeyInfoReference = KeyInfoReference::fromXML($this->xmlRepresentation->documentElement);
+
+        $this->assertEquals('#_e395489e5f8444f1aabb4b2ca98a23b793d211ddf0', $KeyInfoReference->getURI());
+        $this->assertEquals('abc123', $KeyInfoReference->getId());
+    }
+}
