0f6a612ff97743e14b98ed7778bf0e7165ce5e17
Tim van Dijen <tvdijen@gmail.com>
2022-09-17 11:01:13 +0200

    Add ds:SignatureProperty element (#33)
    * Add ds:SignatureProperty element
    
    * Fix oopsie
    
    * Add assertion
    
    * Update PR & schema validation

---
+++ tests/XML/ds/SignaturePropertyTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XMLSecurity\Test\XML\ds;
+
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Test\XML\SchemaValidationTestTrait;
+use SimpleSAML\Test\XML\SerializableElementTestTrait;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XMLSecurity\Constants as C;
+use SimpleSAML\XMLSecurity\XML\ds\SignatureProperty;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\XMLSecurity\Test\XML\ds\SignaturePropertyTest
+ *
+ * @covers \SimpleSAML\XMLSecurity\XML\ds\SignatureProperty
+ * @covers \SimpleSAML\XMLSecurity\XML\ds\AbstractDsElement
+ *
+ * @package simplesamlphp/saml2
+ */
+final class SignaturePropertyTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public function setUp(): void
+    {
+        $this->testedClass = SignatureProperty::class;
+
+        $this->schema = dirname(dirname(dirname(dirname(__FILE__)))) . '/schemas/xmldsig1-schema.xsd';
+
+        $this->xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(dirname(dirname(__FILE__))) . '/resources/xml/ds_SignatureProperty.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $document = DOMDocumentFactory::fromString(
+            '<ssp:HSMSerialNumber xmlns:ssp="urn:x-simplesamlphp:namespace">1234567890</ssp:HSMSerialNumber>'
+        );
+
+        $signatureProperty = new SignatureProperty([new Chunk($document->documentElement)], 'https://simplesamlphp.org/some/target', 'abc123');
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($signatureProperty)
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $signatureProperty = SignatureProperty::fromXML($this->xmlRepresentation->documentElement);
+        $this->assertEquals('https://simplesamlphp.org/some/target', $signatureProperty->getTarget());
+        $this->assertEquals('abc123', $signatureProperty->getId());
+
+        $children = $signatureProperty->getElements();
+        $this->assertCount(1, $children);
+
+        $child = $children[0];
+        $this->assertEquals('HSMSerialNumber', $child->getLocalName());
+        $this->assertEquals('ssp', $child->getPrefix());
+        $this->assertEquals('urn:x-simplesamlphp:namespace', $child->getNamespaceURI());
+        $this->assertEquals('1234567890', $child->getXML()->textContent);
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($signatureProperty),
+        );
+    }
+}
