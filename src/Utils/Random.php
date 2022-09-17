d8f1ac7020120ca28dff5e065bf6d7ddceb79e84
Tim van Dijen <tvdijen@gmail.com>
2022-09-17 17:14:54 +0200

    Remove unused code

--- src/Utils/Random.php
+++ src/Utils/Random.php
@@ -49,19 +49,4 @@ class Random
             );
         }
     }
-
-
-    /**
-     * Generate a globally unique identifier.
-     *
-     * @param string $prefix Prefix to be prepended to the identifier.
-     *
-     * @return string A random globally unique identifier.
-     */
-    public static function generateGUID(string $prefix = '_'): string
-    {
-        $uuid = bin2hex(self::generateRandomBytes(16));
-        return $prefix . substr($uuid, 0, 8) . '-' . substr($uuid, 8, 4) . '-' . substr($uuid, 12, 4) . '-'
-            . substr($uuid, 16, 4) . '-' . substr($uuid, 20, 12);
-    }
 }
