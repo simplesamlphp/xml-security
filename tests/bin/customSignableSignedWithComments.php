216bcc0bae10214ba798ea3bed3bad644490436a
Tim van Dijen <tvdijen@gmail.com>
2022-12-03 13:33:28 +0100

    Regenerate custom_ test xmls

---
+++ tests/bin/customSignableSignedWithComments.php
@@ -0,0 +1,37 @@
+#!/usr/bin/env php
+<?php
+
+require_once(dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php');
+
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XMLSecurity\Alg\Signature\SignatureAlgorithmFactory;
+use SimpleSAML\XMLSecurity\Constants as C;
+use SimpleSAML\XMLSecurity\TestUtils\PEMCertificatesMock;
+use SimpleSAML\XMLSecurity\Test\XML\CustomSignable;
+use SimpleSAML\XMLSecurity\XML\ds\KeyInfo;
+use SimpleSAML\XMLSecurity\XML\ds\X509Certificate;
+use SimpleSAML\XMLSecurity\XML\ds\X509Data;
+
+$document = DOMDocumentFactory::fromFile(dirname(dirname(__FILE__)) . '/resources/xml/custom_CustomSignable.xml');
+
+$chunks = $document->documentElement->getElementsByTagNameNS("urn:x-simplesamlphp:namespace", "Chunk");
+$chunk = $chunks->item(0);
+$chunkContent = $chunk->firstChild;
+$chunk->insertBefore(new DOMComment('comment'), $chunkContent);
+$chunk->appendChild(new DOMComment('comment'));
+
+$signer = (new SignatureAlgorithmFactory())->getAlgorithm(
+    C::SIG_RSA_SHA256,
+    PEMCertificatesMock::getPrivateKey(PEMCertificatesMock::SELFSIGNED_PRIVATE_KEY),
+);
+
+$keyInfo = new KeyInfo([
+    new X509Data([
+         new X509Certificate(PEMCertificatesMock::getPlainCertificateContents(PEMCertificatesMock::SELFSIGNED_CERTIFICATE))
+    ])
+]);
+
+$unsignedElement = CustomSignable::fromXML($document->documentElement);
+$unsignedElement->sign($signer, C::C14N_EXCLUSIVE_WITH_COMMENTS, $keyInfo);
+
+echo $unsignedElement->toXML()->ownerDocument->saveXML();
---
+++ tests/bin/customSignableSignedWithId.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+
+require_once(dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php');
+
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XMLSecurity\Alg\Signature\SignatureAlgorithmFactory;
+use SimpleSAML\XMLSecurity\Constants as C;
+use SimpleSAML\XMLSecurity\TestUtils\PEMCertificatesMock;
+use SimpleSAML\XMLSecurity\Test\XML\CustomSignable;
+use SimpleSAML\XMLSecurity\XML\ds\KeyInfo;
+use SimpleSAML\XMLSecurity\XML\ds\X509Certificate;
+use SimpleSAML\XMLSecurity\XML\ds\X509Data;
+
+$document = DOMDocumentFactory::fromFile(dirname(dirname(__FILE__)) . '/resources/xml/custom_CustomSignable.xml');
+$document->documentElement->setAttribute('id', '_1234');
+$signer = (new SignatureAlgorithmFactory())->getAlgorithm(
+    C::SIG_RSA_SHA256,
+    PEMCertificatesMock::getPrivateKey(PEMCertificatesMock::SELFSIGNED_PRIVATE_KEY),
+);
+
+$keyInfo = new KeyInfo([
+    new X509Data([
+         new X509Certificate(PEMCertificatesMock::getPlainCertificateContents(PEMCertificatesMock::SELFSIGNED_CERTIFICATE))
+    ])
+]);
+
+$unsignedElement = CustomSignable::fromXML($document->documentElement);
+$unsignedElement->sign($signer, C::C14N_EXCLUSIVE_WITHOUT_COMMENTS, $keyInfo);
+echo $unsignedElement->toXML()->ownerDocument->saveXML();
