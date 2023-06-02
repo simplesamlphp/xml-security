40f4d1579a0cd0df1e99a0e60331473e35962e9f
Tim van Dijen <tvdijen@gmail.com>
2023-06-02 13:46:20 +0200

    Add missing break

--- src/XML/EncryptedElementTrait.php
+++ src/XML/EncryptedElementTrait.php
@@ -47,6 +47,7 @@ trait EncryptedElementTrait
         foreach ($keyInfo->getInfo() as $info) {
             if ($info instanceof EncryptedKey) {
                 $this->encryptedKey = $info;
+                break;
             }
         }
     }
