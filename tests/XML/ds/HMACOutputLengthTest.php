310db4eb24f9c9211883217bf9ac054f1cb866ae
Tim van Dijen <tvdijen@gmail.com>
2024-11-29 23:40:29 +0100

    Add HMACOutputLength element

---
+++ tests/XML/ds/HMACOutputLengthTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XMLSecurity\Test\XML\ds;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+use SimpleSAML\XMLSecurity\XML\ds\AbstractDsElement;
+use SimpleSAML\XMLSecurity\XML\ds\HMACOutputLength;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\XMLSecurity\Test\XML\ds\HMACOutputLengthTest
+ *
+ * @package simplesamlphp/xml-security
+ */
+#[CoversClass(AbstractDsElement::class)]
+#[CoversClass(HMACOutputLength::class)]
+final class HMACOutputLengthTest extends TestCase
+{
+    use SerializableElementTestTrait;
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$testedClass = HMACOutputLength::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 3) . '/resources/xml/ds_HMACOutputLength.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $hmacOutputLength = new HMACOutputLength('1234');
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($hmacOutputLength),
+        );
+    }
+}
