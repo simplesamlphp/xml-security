f489162a79f2f968f47b337bbb8873995a2185cc
Tim van Dijen <tvdijen@gmail.com>
2024-12-08 01:01:24 +0100

    Feature: Add support for XML-encryption 1.1 elements (#59)
    * Add xenc-11 schema + namespace const
    
    * Add namespace-abstract
    
    * Add xenc11:KeyDerivationMethod element
    
    * Add xenc11:ConcatKDFParams element
    
    * Add xenc11:DerivedKeyName element
    
    * Add xenc11:MasterKeyName element
    
    * Add xenc11:Specified element
    
    * Add xenc11:Parameters element
    
    * Add xenc11:OtherSource element
    
    * Add xenc11:PRF element
    
    * Add xenc11:KeyLength element
    
    * Add xenc11:IterationCount element
    
    * Exclude empty parameters
    
    * Add xenc11:MGF element
    
    * Add xenc11:Salt element
    
    * Add xenc11:PBKDF2-params element
    
    * Add xenc11:DerivedKey element

---
+++ tests/XML/xenc11/OtherSourceTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\xenc11;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\XML\Attribute as XMLAttribute;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+use SimpleSAML\XMLSecurity\XML\xenc11\AbstractAlgorithmIdentifierType;
+use SimpleSAML\XMLSecurity\XML\xenc11\AbstractXenc11Element;
+use SimpleSAML\XMLSecurity\XML\xenc11\OtherSource;
+use SimpleSAML\XMLSecurity\XML\xenc11\Parameters;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\XMLSecurity\XML\xenc11\OtherSourceTest
+ *
+ * @package simplesamlphp/xml-security
+ */
+#[CoversClass(OtherSource::class)]
+#[CoversClass(AbstractAlgorithmIdentifierType::class)]
+#[CoversClass(AbstractXenc11Element::class)]
+final class OtherSourceTest extends TestCase
+{
+    use SerializableElementTestTrait;
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$testedClass = OtherSource::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 3) . '/resources/xml/xenc11_OtherSource.xml',
+        );
+    }
+
+
+    // marshalling
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $chunk = new Chunk(DOMDocumentFactory::fromString(
+            <<<XML
+    <ssp:AuthenticationContextDeclaration xmlns:ssp="urn:x-simplesamlphp:namespace">
+      <ssp:Identification nym="verinymity">
+        <ssp:Extension>
+          <ssp:NoVerification/>
+        </ssp:Extension>
+      </ssp:Identification>
+    </ssp:AuthenticationContextDeclaration>
+XML
+            ,
+        )->documentElement);
+
+        $parameters = new Parameters(
+            [$chunk],
+            [new XMLAttribute('urn:x-simplesamlphp:namespace', 'ssp', 'attr1', 'testval1')],
+        );
+
+        $otherSource = new OtherSource('urn:x-simplesamlphp:algorithm', $parameters);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($otherSource),
+        );
+    }
+}
