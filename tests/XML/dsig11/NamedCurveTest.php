486e5ec68e42d63efff805080aabd97713e2f606
Tim van Dijen <tvdijen@gmail.com>
2025-06-09 00:36:33 +0200

    Add dsig11:NamedCurve-element

---
+++ tests/XML/dsig11/NamedCurveTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XMLSecurity\Test\XML\dsig11;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+use SimpleSAML\XMLSecurity\XML\dsig11\AbstractDsig11Element;
+use SimpleSAML\XMLSecurity\XML\dsig11\AbstractNamedCurveType;
+use SimpleSAML\XMLSecurity\XML\dsig11\NamedCurve;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\XMLSecurity\Test\XML\dsig11\NamedCurveTest
+ *
+ * @package simplesamlphp/xml-security
+ */
+#[CoversClass(AbstractDsig11Element::class)]
+#[CoversClass(AbstractNamedCurveType::class)]
+#[CoversClass(NamedCurve::class)]
+final class NamedCurveTest extends TestCase
+{
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$testedClass = NamedCurve::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 3) . '/resources/xml/dsig11_NamedCurve.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $namedCurve = new NamedCurve('urn:x-simplesamlphp:URI');
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($namedCurve),
+        );
+    }
+}
