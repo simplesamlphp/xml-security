a91a98685b860f3cd069f2e6ff28eaa0f787de6c
Tim van Dijen <tvdijen@gmail.com>
2025-07-26 17:00:06 +0200

    Add missing extension to list of direct dependencies

--- src/Utils/Random.php
+++ src/Utils/Random.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace SimpleSAML\XMLSecurity\Utils;
 
-use Random\RandomException;
+use Exception;
 use SimpleSAML\Assert\Assert;
 use SimpleSAML\XMLSecurity\Exception\InvalidArgumentException;
 use SimpleSAML\XMLSecurity\Exception\RuntimeException;
@@ -43,7 +43,7 @@ class Random
             return random_bytes($length);
         } catch (ValueError) { // @phpstan-ignore-line
             throw new InvalidArgumentException('Invalid length received to generate random bytes.');
-        } catch (RandomException) {
+        } catch (Exception) {
             throw new RuntimeException(
                 'Cannot generate random bytes, no cryptographically secure random generator available.',
             );
